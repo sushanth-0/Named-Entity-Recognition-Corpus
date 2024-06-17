20549927d7782715c9dfed88ac30f7fc15c49dfc
sushanth-0 <102034708+sushanth-0@users.noreply.github.com>
2024-06-17 07:52:23 -0400

    initial setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="named entitiy recognition",
+    version="1",
+    author="Suhsanth",
+    author_email="rachamreddy.sushanth@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
